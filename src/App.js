0edca554d6e5187da446e52ab24e4b4d937dce79
MojeedB <mojeedbello101@yahoo.com>
2022-05-02 16:29:41 -0400

    App.js Modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import { NewsContextProvider } from './component/news/NewsContext';
+import Footer from './component/pagecomponents/Footer';
+import Navigation from './component/pagecomponents/Navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import AboutUsPage from './pages/AboutUsPage';
+import DiscussionPage from './pages/DiscussionPage';
+import HomePage from './pages/HomePage';
+import TrendingPage from './pages/TrendingPage';
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+
+      <Router>
+        <NewsContextProvider>
+          <Navigation />
+          <div className="container px-0">
+            <div className="row d-flex justify-content-center">
+              <Routes>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/trending" element={<TrendingPage />} />
+                <Route path="/discussion" element={<DiscussionPage />} />
+                <Route path="/about" element={<AboutUsPage />} />
+                {/* Route to landing page for testing*/}
+                {/* <Route path="/landing" element={<LandingPage />} /> */}
+              </Routes>
+            </div>
+          </div>
+          <Footer />
+        </NewsContextProvider>
+      </Router>
+    );
+  }
 }
 
 export default App;
